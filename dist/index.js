6417e5357cd015c840bff90bc33e1764552dea8e
Sokolov Andrew <webdeveloper@my.com>
2020-12-17 16:55:02 +0300

    Minot changes

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,3 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -94,5 +87,4 @@ var Metrics = /*#__PURE__*/function () {
 }();
 
 window.Metrics = Metrics;
-var _default = Metrics;
-exports["default"] = _default;
+export default Metrics;
